3921bb851d8c40c335d57918a7577a878ee6fa86
Pawlik Void <gfryng@gmail.com>
2017-08-23 22:38:25 +0200

    Added binary to final pack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+var nodeExternals = require('webpack-node-externals');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path')
+
 module.exports = {
   entry: './handler.js',
   target: 'node',
+  externals: [nodeExternals()],
+  plugins: [new CopyWebpackPlugin([{
+    from: path.join(__dirname, "/bin/phantomjs-linux"), to: "bin/"
+  }]) ],
   module: {
     loaders: []
   }
